267a770afb9310cdd32f7b85969a59bf06a34978
abhishekkarthik <karthik95abi@gmail.com>
2024-06-19 22:12:11 -0300

    First or initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, ["Dog"]),
+    React.createElement("h3", {}, ["Husky"]),
+    React.createElement("h3", {}, ["5 years old"]),
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hello World"),
+    React.createElement(Pet),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
